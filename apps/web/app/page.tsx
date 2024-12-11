4457d9797d79e5723fb15084177df7f487f5f6cb
Gabriel Costa Luiz <53578058+GabrielCostaLuiz@users.noreply.github.com>
2024-12-10 23:09:32 -0300

    Cria layout e page do frontend para sinalizar que está em construção

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -2,8 +2,19 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Em Construção</h1>
+    <div className="min-h-screen flex text-center justify-center items-center bg-white text-black">
+      <main className="min-h-fit space-y-10">
+        <h1 className=" text-6xl font-bold">RepoShift</h1>
+
+        <p className="w-3/4 mx-auto text-lg">
+          Estou criando, neste exato momento, um novo espaço na internet para
+          quem busca agilidade na construção de seu portfólio. Neste projeto,
+          busco unir precisão e empatia, com um único objetivo: ajudar as
+          pessoas a alcançarem seus objetivos com rapidez e eficiência.
+        </p>
+
+        <p className="text-red-500 font-bold text-lg">Em construção !!!!</p>
+      </main>
     </div>
   );
 }
