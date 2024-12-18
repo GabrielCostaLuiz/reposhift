1daf54886b4685adb8000fc5e45ae189240ef402
gabrielcostaluiz <gabrielbragacostaluiz@gmail.com.br>
2024-12-17 22:14:59 -0300

    test: teste Preview

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,6 +1,5 @@
 export default function Home() {
   return (
-<<<<<<< HEAD
     <div className="flex min-h-screen items-center justify-center bg-white text-center text-black">
       <main className="min-h-fit space-y-10">
         <h1 className=" text-6xl font-bold">RepoShift</h1>
@@ -9,15 +8,14 @@ export default function Home() {
           Estou criando, neste exato momento, um novo espaço na internet para
           quem busca agilidade na construção de seu portfólio. Neste projeto,
           busco unir precisão e empatia, com um único objetivo: ajudar as
-          pessoas a alcançarem seus objetivos com rapidez e eficiência.
+          pessoas a alcançarem seus objetivos com rapidez e eficiência. Teste
+          Preview
         </p>
 
-        <p className="text-lg font-bold text-red-500">Em construção !!!! teste preview</p>
+        <p className="text-lg font-bold text-red-500">
+          Em construção !!!! teste preview
+        </p>
       </main>
-=======
-    <div>
-      <h1>Em Construção</h1>
->>>>>>> ca8c75a (chore: cria a pagina em construção do nextjs)
     </div>
   )
 }
